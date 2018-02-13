925260916f2bafc0a698da1ee7e283bbd49dee61
Zach Gavin <email@email.com>
2018-02-12 19:59:18 -0500

    Removed node module, fixed folder structure for heroku

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').load({path: '../.env'});
+}
+const API_KEY = process.env.MSW_API_KEY;
+
+const router = express.Router();
+
+router.get('/msw/384', function (req, res) {
+	const url = `https://magicseaweed.com/api/${API_KEY}/forecast/?spot_id=384`;
+	request(url, (err, response, body) => {
+		res.send({express: body});
+	});
+})
+
+router.get('/msw/383', function (req, res) {
+	const url = `https://magicseaweed.com/api/${API_KEY}/forecast/?spot_id=383`;
+	request(url, (err, response, body) => {
+		res.send({express: body});
+	});
+})
+
+app.use(router);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Example app  on port ${port}!`))
