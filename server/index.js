11f863f5532359c0ee30897e89bc7d8a2734f43b
Zach Gavin <email@email.com>
2018-02-26 15:56:42 -0500

    remove compression

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(sslRedirect);
 }
 
-app.use(compression());
+// app.use(compression());
 
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
