546c306e195fc7eaafc4e9bec173d570254dd8cd
Zach Gavin <email@email.com>
2018-02-26 14:58:53 -0500

    Changing some server logic

--- server/index.js
+++ server/index.js
@@ -4,6 +4,26 @@ const request = require('request');
 const path = require('path');
 const compression = require('compression');
 
+const sslRedirect = () => {
+	function requestIsSecure(req) {
+		return req.header('x-forwarded-proto') === 'https' || req.secure === true;
+	}
+
+	return (req, res, next) => {
+		if (requestIsSecure(req)) {
+			next();
+		} else {
+			res.redirect(301, `https://${req.header('host')}${req.url}`);
+		}
+	};
+};
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(sslRedirect);
+}
+
+app.use(compression());
+
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -12,11 +32,10 @@ app.use(function(req, res, next) {
 
 app.use(express.static(path.resolve(__dirname, '../react/build')));
 
-app.use(compression());
-
 if (process.env.NODE_ENV !== 'production') {
 	const r = require('dotenv').load();
 }
+
 const API_KEY = process.env.MSW_API_KEY;
 const router = express.Router();
 
@@ -34,30 +53,13 @@ router.get('/msw/383', function (req, res) {
 	});
 })
 
-
 app.use(router);
 
 app.get('/', function(request, response) {
 	response.sendFile(path.resolve(__dirname, '../react/build', 'index.html'));
 });
 
-const sslRedirect = () => {
-  function requestIsSecure(req) {
-    return req.header('x-forwarded-proto') === 'https' || req.secure === true;
-  }
 
-  return (req, res, next) => {
-    if (requestIsSecure(req)) {
-      next();
-    } else {
-      res.redirect(301, `https://${req.header('host')}${req.url}`);
-    }
-  };
-};
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(sslRedirect);
-}
 
 const port = process.env.PORT || 5000;
 
