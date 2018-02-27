055c96ac66966ca62e59ad00f7a09d884e19e35f
Zach Gavin <email@email.com>
2018-02-27 10:51:34 -0500

    moved security redirect to top

--- server/index.js
+++ server/index.js
@@ -4,7 +4,23 @@ const request = require('request');
 const path = require('path');
 const compression = require('compression');
 
+const sslRedirect = () => {
+	function requestIsSecure(req) {
+		return req.header('x-forwarded-proto') === 'https' || req.secure === true;
+	}
 
+	return (req, res, next) => {
+		if (requestIsSecure(req)) {
+			next();
+		} else {
+			res.redirect(301, `https://${req.header('host')}${req.url}`);
+		}
+	};
+};
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(sslRedirect);
+}
 // app.use(compression());
 
 app.use(function(req, res, next) {
@@ -43,25 +59,6 @@ app.use(router);
 app.get('/', function(request, response) {
 	response.sendFile(path.resolve(__dirname, '../react/build', 'index.html'));
 });
-
-const sslRedirect = () => {
-	function requestIsSecure(req) {
-		return req.header('x-forwarded-proto') === 'https' || req.secure === true;
-	}
-
-	return (req, res, next) => {
-		if (requestIsSecure(req)) {
-			next();
-		} else {
-			res.redirect(301, `https://${req.header('host')}${req.url}`);
-		}
-	};
-};
-
-if (process.env.NODE_ENV === 'production') {
-	app.use(sslRedirect);
-}
-
 
 const port = process.env.PORT || 5000;
 
