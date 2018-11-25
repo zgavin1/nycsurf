55cc356bc3dfb3b980548228b52b40707939d956
Zach Gavin <email@email.com>
2018-11-25 12:40:38 -0600

    Add funtional App2 file with state hooks.

---
+++ react/src/App2.js
@@ -0,0 +1,68 @@
+
+import React, { useState, useEffect } from 'react';
+import moment from 'moment'
+import './App.css';
+import { beaches, evaluateRating } from './utils.js';
+import logo from './msw_powered_by.png';
+
+
+function App() {
+	const [forecast, setForecast] = useState({});
+	const [currentBeach, setCurrentBeach] = useState(beaches.rockaway);
+	
+	const fetchData = async () => {
+		const res = await fetch(`/msw/${currentBeach.beachId}`);
+		const body = await res.json();
+
+		setForecast(
+			{
+	      ...forecast,
+	      ...JSON.parse(body.express)
+	    }
+		);
+	}
+
+	useEffect(() => {
+	  fetchData()
+	});
+
+	if (Object.keys(forecast).length) {
+		const forecastToday = forecast[4];
+		const forecastTomorrow = forecast[12];
+		var timeToday = moment(forecastToday.localTimestamp*1000).format("ddd, ha");
+		var timeTomorrow = moment(forecastTomorrow.localTimestamp*1000).format("ddd, ha");
+	
+		var statusToday = evaluateRating(forecastToday.solidRating, forecastToday.fadedRating)
+		var statusTomorrow = evaluateRating(forecastTomorrow.solidRating, forecastTomorrow.fadedRating)
+	}
+	
+	return (
+		<div className="nycsurf">
+			<header className="nycsurf-header">
+				<h1 className="nycsurf-title">{currentBeach.name}</h1>
+			</header>
+			<aside className="nycsurf-aside">
+				<button type="button" id="beachToggle" onClick={() => setCurrentBeach(currentBeach === beaches.long ? beaches.rockaway : beaches.long)}>Toggle Beach</button>
+			</aside>
+			<aside className="nycsurf-aside nycsurf-aside--alt">
+				<a href="https://magicseaweed.com"><img alt="Link to Magic Seaweed" src={logo} /></a>
+			</aside>
+			{
+				!Object.keys(forecast).length
+				? null
+				: <div className="nycsurf-main">
+						<section>
+							<h2>{timeToday}</h2>
+							<p>{statusToday}</p>
+						</section>
+						<section>
+							<h2>{timeTomorrow}</h2>
+							<p>{statusTomorrow}</p>
+						</section>
+					</div>
+			}
+		</div>
+	);
+}
+
+export default App;
--- react/src/index.js
+++ react/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './App2.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
